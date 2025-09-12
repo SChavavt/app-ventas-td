101f9eb94a19634821aa11b04c1f2a26f4d523f3
SChavavt <134571321+SChavavt@users.noreply.github.com>
2025-09-12 11:39:50 -0600

    cache pedidos combinados

--- app_v.py
+++ app_v.py
@@ -1107,7 +1107,7 @@ with tab1:
 
 
 
-@st.cache_data(ttl=30)
+@st.cache_data(ttl=300)
 def cargar_pedidos_combinados():
     """
     Carga y unifica pedidos de 'datos_pedidos' y 'casos_especiales'.
@@ -1268,6 +1268,8 @@ if "reset_inputs_tab2" in st.session_state:
 
 with tab2:
     st.header("✏️ Modificar Pedido Existente")
+    if st.button("🔄 Actualizar pedidos"):
+        cargar_pedidos_combinados.clear()
 
     message_placeholder_tab2 = st.empty()
 
